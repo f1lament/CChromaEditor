dd5746132f36ebe882b0e61a58974a27d26ef000
Chu <cheehiong@razer.com>
2022-04-19 13:56:46 +0800

    Try 2019 community version.

--- build_script.bat
+++ build_script.bat
@@ -1,4 +1,4 @@
 call nuget.exe restore "CChromaEditor.sln"
-call "%programfiles(x86)%\Microsoft Visual Studio\2017\Professional\Common7\Tools\VsDevCmd.bat"
+call "%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Community\Common7\Tools\VsDevCmd.bat"
 
 msbuild.exe /m CChromaEditor.sln /t:CChromaEditorLibrary /p:configuration=release;platform=x86
