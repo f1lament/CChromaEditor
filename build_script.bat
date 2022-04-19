16e08e5287f89be7fa99d85a2a069274d63f65ea
Chu <cheehiong@razer.com>
2022-04-19 13:21:21 +0800

    Reverted last change.

--- build_script.bat
+++ build_script.bat
@@ -1,3 +1,4 @@
 call nuget.exe restore "CChromaEditor.sln"
+call "%programfiles(x86)%\Microsoft Visual Studio 14.0\Common7\Tools\VsMSBuildCmd.bat"
 
 msbuild.exe /m CChromaEditor.sln /t:CChromaEditorLibrary /p:configuration=release;platform=x86
