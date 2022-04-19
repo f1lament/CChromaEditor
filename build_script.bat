43a6e90382a7abde85b353d20ceb83e9756c9f80
Chu <cheehiong@razer.com>
2022-04-19 13:50:55 +0800

    Use Visual Studio 14.0 environment.

--- build_script.bat
+++ build_script.bat
@@ -1,4 +1,4 @@
 call nuget.exe restore "CChromaEditor.sln"
-call "%programfiles(x86)%\Microsoft Visual Studio 14.0\Common7\Tools\VsMSBuildCmd.bat"
+call "%programfiles(x86)%\Microsoft Visual Studio\2017\Professional\Common7\Tools\VsDevCmd.bat"
 
 msbuild.exe /m CChromaEditor.sln /t:CChromaEditorLibrary /p:configuration=release;platform=x86
